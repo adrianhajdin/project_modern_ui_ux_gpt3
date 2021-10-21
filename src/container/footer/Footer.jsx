924cb08105979223b32aeea01fabfa58815a356a
Tidbits JS <tidbitsjs@gmail.com>
2021-10-21 19:39:44 +0530

    gpt3 footer section

---
+++ src/container/footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import gpt3Logo from '../../logo.svg';
+import './footer.css';
+
+const Footer = () => (
+  <div className="gpt3__footer">
+    <div className="gpt3__footer-heading">
+      <h1>Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+        <p>085-132567</p>
+        <p>info@payme.net</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2021 GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
