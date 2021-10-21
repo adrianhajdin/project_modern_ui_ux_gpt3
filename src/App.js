f1746a39a2253942eaedc1b21d810f9491ede1a6
Tidbits JS <tidbitsjs@gmail.com>
2021-10-21 23:14:37 +0530

    cta component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Footer from './container/footer/Footer';
 import './App.css';
 import Blog from './container/blog/Blog';
+import CTA from './components/cta/CTA';
 
 function App() {
   return (
     <div className="App">
+      <CTA />
       <Blog />
       <Footer />
     </div>
---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './cta.css';
+
+const CTA = () => (
+  <div className="gpt3__cta">
+    <div className="gpt3__cta-content">
+      <p>Request Early Access to Get Started</p>
+      <h3>Register Today & start exploring the endless possibilities.</h3>
+    </div>
+    <div className="gpt3__cta-btn">
+      <button type="button">Get Started</button>
+    </div>
+  </div>
+);
+
+export default CTA;
