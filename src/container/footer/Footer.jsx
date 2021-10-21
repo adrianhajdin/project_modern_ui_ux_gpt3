7b705bed7bee974f6fdaf456b84d972871207378
Tidbits JS <tidbitsjs@gmail.com>
2021-10-21 22:38:40 +0530

    footer fix

--- src/container/footer/Footer.jsx
+++ src/container/footer/Footer.jsx
@@ -8,6 +8,10 @@ const Footer = () => (
       <h1>Do you want to step in to the future before others</h1>
     </div>
 
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
