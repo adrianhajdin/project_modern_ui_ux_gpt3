e9c53b5b8e24d1b070e39baa156c60fdda31fce1
Kuldeep-sahu6260 <79197859+Kuldeep-sahu6260@users.noreply.github.com>
2022-03-07 23:12:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './contain
 import { CTA, Brand, Navbar } from './components';
 
 import './App.css';
-
+// kuldeep
 const App = () => (
   <div className="App">
     <div className="gradient__bg">
