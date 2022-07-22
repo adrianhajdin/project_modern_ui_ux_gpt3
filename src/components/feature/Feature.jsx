deef721ef1c6ec60c99fd595eef29cbf581e4e2f
Zakarie Yaris <zakariaali350@gmail.com>
2022-07-22 21:06:24 +0100

    Update Feature.jsx

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -4,7 +4,6 @@ import './feature.css';
 const Feature = ({ title, text }) => (
   <div className="gpt3__features-container__feature">
     <div className="gpt3__features-container__feature-title">
-      <div />
       <h1>{title}</h1>
     </div>
     <div className="gpt3__features-container_feature-text">
